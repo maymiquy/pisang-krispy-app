f93a3c9f46da921415e111a91ad467f7cd933d52
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-01-18 22:39:15 +0700

    setup: react ts project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Nabar";
+import Footer from "./components/Footer";
+
+function App() {
+ return (
+  <div className="min-h-screen bg-neutral-200">
+   <Navbar />
+   <Footer />
+  </div>
+ );
+}
+
+export default App;
